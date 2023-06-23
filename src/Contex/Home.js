e3fea7735cb576a25bf417a407d302fcd7bf9cf8
Guptaabhii <112747867+Guptaabhii@users.noreply.github.com>
2023-06-23 21:47:32 +0530

    added search by name functionality

--- src/Contex/Home.js
+++ src/Contex/Home.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { useSnacksContext } from "./SnacksContext";
 
 export const ProductsList = () => {
+  const [search, setSearch] = useState("");
+
   const {
     products,
     setProducts,
@@ -10,10 +13,23 @@ export const ProductsList = () => {
     sortByCalories,
     sortByPrice,
   } = useSnacksContext();
+  const filterBySearch =
+    search?.length >= 0
+      ? products?.filter((item) =>
+          item.product_name.toLowerCase().includes(search.toLocaleLowerCase())
+        )
+      : products;
   return (
     <div>
-      <div>
+      <div className="flex flex-col items-center">
         <h1>Snacks Table</h1>
+        <input
+          type="text"
+          placeholder="filter by name"
+          className="border border-solid border-black"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
       </div>
       <table className="flex flex-col justify-center gap-4">
         <tr className="flex  justify-center gap-8 cursor-pointer">
@@ -62,17 +78,17 @@ export const ProductsList = () => {
           >
             Calories
           </th>
-          <th className="w-4 mx-8">Ingredients</th>
+          <th className="w-8 mx-8">Ingredients</th>
         </tr>
 
-        {products.map((item) => (
+        {filterBySearch.map((item) => (
           <tr key={item.id} className="flex  justify-center gap-8">
             <td className="w-4 mx-8">{item.id}</td>
             <td className="w-4 mx-8">{item.product_name}</td>
             <td className="w-4 mx-8">{item.product_weight}</td>
             <td className="w-4 mx-8">{item.price}</td>
             <td className="w-4 mx-8">{item.calories}</td>
-            <td className="w-4 mx-8">{item.ingredients}</td>
+            <td className="w-4 mx-8">{item.ingredients.join(",")}</td>
           </tr>
         ))}
       </table>
